563fedffd5db5108734c0e9764f4e0d5a928f8ae
DS4506 <davidsmith4506@gmail.com>
2025-12-15 20:26:06 -0600

    Initial Commit

---
+++ Stadium Sight Words/Stadium_Sight_WordsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stadium_Sight_WordsApp.swift
+//  Stadium Sight Words
+//
+//  Created by Willie Earl on 12/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Stadium_Sight_WordsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
